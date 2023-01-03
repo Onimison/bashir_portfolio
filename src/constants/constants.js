d88d342779d252eee1d5910118f0b991319a9bfb
Onimison <godblessbashir@gmail.com>
2023-01-03 01:35:15 +0100

    feat: updated the list of projects in the constant component

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,30 @@
 export const projects = [
   {
-    title: '10.8.8 Website',
-    description: "Using React and Tailwind CSS I built the website to showcase the work of a magazine publishing startup. ",
-      image: '/images/10.png',
-      tags: ['React', 'TailwindCSS'],
-    source: 'https://google.com',
-    visit: 'https://1088africa.netlify.app/',
+    title: 'Zuri-Chat Web',
+    description: "Contributed to maintaining the Zuri-chat web app as part of my HNGi9 task",
+      image: '/images/zurichat.png',
+      tags: ['ReactJs, Tailwind CSS'],
+    visit: 'https://github.com/Onimison/zc_main',
+    source: 'https://www.zuri.chat/',
     id: 0,
+  },
+  {
+    title: 'Portfolio Website',
+    description: "Using NextJs and styled Components, I built my portfolio website to showcase my projects which I then deployed to Vercel.",
+      image: '/images/portfolio.png',
+      tags: ['reactJs, NextJs'],
+    visit: 'https://github.com/Onimison/bashir_portfolio',
+    source: 'https://bashir-portfolio.vercel.app/',
+    id: 1,
+  },
+  {
+    title: '10.8.8 Website',
+    description: "Using vanilla JavaScript, I built the website to showcase the work of a magazine publishing startup. ",
+      image: '/images/10.png',
+      tags: ['JavaScript'],
+    visit: 'https://github.com/Onimison/1088africa',
+    source: 'https://1088africa.netlify.app/',
+    id: 2,
   },
 
   {
@@ -14,13 +32,15 @@ export const projects = [
     description: "Using React and Material UI I built the login and sign up pages as part of my task for the project phase of my I4GxZuri Internship. ",
       image: '/images/11.png',
       tags: ['React', 'Material UI'],
-    source: 'https://google.com',
-    visit: '',
-    id: 1,
+    visit: 'https://github.com/zuri-training/Anime-lib_T94-frontend-/tree/feature-signup-login',
+    source: '',
+    id: 3,
   }
 ];
 
 export const TimeLineData = [
   { year: 2021, text: 'Started my journey', },
-  { year: 2022, text: 'Worked as the Lead developer at 10.8.8 Africa', }
+  { year: 2022, text: 'Worked as the Lead developer at 10.8.8 Africa', },
+  { year: 2022, text: 'Completed I4GxZuri internship in August 2022', },
+  { year: 2022, text: 'Reached stage of the HNGi9 internship in December 2022', }
 ];
