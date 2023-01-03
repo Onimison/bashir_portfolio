594c046cc945eadf1acef498bd9b566e24b5ad6d
Onimison <godblessbashir@gmail.com>
2023-01-03 01:31:28 +0100

    fix: repaired the button on the hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,12 +8,12 @@ const Hero = () => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main center>
-        Hey there, <br />
+        Yoo, <br />
         I'm Bashir
       </SectionTitle>
       <SectionText>
         I am a Front-End Developer </SectionText>
-      <Button onclick={() => window.location = "https://google.com" }>Download Resume</Button>
+      <Button onClick={() => window.location.href = "https://drive.google.com/file/d/1tQB0xeLfL0SPOb3CIwLfvhGo02_rjdKv/view?usp=share_link" }>Resume</Button>
     </LeftSection>
   </Section>
 );
