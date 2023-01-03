31a5e34300767175955e3c6b7fb16c6b1643325f
Onimison <godblessbashir@gmail.com>
2023-01-03 01:29:46 +0100

    feat-modified the navlinks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,12 @@ import { DiCssdeck } from 'react-icons/di';
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
-const Header = () =>  (
-  <Container>
-    <Div1>
+
+export default function Header() {
+  return (
+    <>
+      <Container>
+      <Div1> 
       <Link href="/">
         <a style={{ display: "flex", alignItems: "center", color: 'white', marginBottom: '20px' }}>
           <DiCssdeck size="3rem" /> <Span>Bashir</Span>
@@ -44,7 +47,7 @@ const Header = () =>  (
       </SocialIcons>
     </Div3>
 
-  </Container>
-);
-
-export default Header;
+    </Container>
+    </>
+  )
+}
