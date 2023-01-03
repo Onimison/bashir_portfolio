07f301fd65e94f67b8f76b737e4db300d846b655
Onimison <godblessbashir@gmail.com>
2023-01-03 01:39:45 +0100

    fix: debugged errors in pages and layout components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,10 +3,10 @@ import Theme from '../styles/theme';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+
       <Theme>
         <Component {...pageProps} />
       </Theme>
-    </>
+
   );
 }
